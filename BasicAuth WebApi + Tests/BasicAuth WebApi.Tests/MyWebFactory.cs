5f58e6894c4abfb179a638467e7e41b9115cd887
Nichita Driuma [Intern] <Nichita.Driuma@endava.com>
2021-09-20 15:21:56 +0300

    add tests

---
+++ BasicAuth WebApi + Tests/BasicAuth WebApi.Tests/MyWebFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAuth_WebApi.Tests
+{
+    public class CustomWebFactory : WebApplicationFactory<Startup> 
+    {
+        protected override IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder().ConfigureWebHost((builder) =>
+            {
+                builder.UseStartup<TestStartup>();
+            });
+        }
+    }
+}
+
---
+++ BasicAuth WebApi + Tests/BasicAuth WebApi.Tests/TestStartup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using BasicAuth_WebApi.Data;
+using BasicAuth_WebApi.Interfaces;
+using BasicAuth_WebApi.Services;
+
+
+namespace BasicAuth_WebApi.Tests
+{
+    public class TestStartup
+    {
+        public TestStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddApplicationPart(typeof(TestStartup).Assembly);
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+
