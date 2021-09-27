4a07b7b0aa058a8e16890e19b9150b6e1abaa9e9
Nikita Driuma <Nichita.Driuma@endava.com>
2021-09-27 11:38:37 +0300

    added xunit with test server

---
+++ BasicAuth WebApi + Tests/BasicAuthWebApi/Interfaces/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BasicAuthWebApi.Models;
+
+namespace BasicAuthWebApi.Interfaces
+{
+    public interface IUserRepository : IDisposable
+    {
+        Task<ActionResult<IEnumerable<User>>> GetAllUsers();
+        
+        Task<User> GetUser(string username);
+
+        Task AddUser(User user);
+
+        Task Save();
+
+    }
+}
---
+++ BasicAuth WebApi + Tests/BasicAuthWebApi/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuthWebApi.Interfaces
+{
+    public interface IUserService
+    {
+        Task<bool> ValidateCredentials(string username, string password);
+
+        string HashPassword(string password);
+
+        Task<bool> UsernameIsBusy(string username);
+    }
+}
